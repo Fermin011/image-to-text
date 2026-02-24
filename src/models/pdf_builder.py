59373f15b211d76f9f416d48e49499b481a58fc9
Fermin011 <ferminoscardasso01@gmail.com>
2026-02-24 06:58:09 -0300

    feat: generador de PDF con texto seleccionable

--- src/models/pdf_builder.py
+++ src/models/pdf_builder.py
@@ -1,2 +1,28 @@
+from PyPDF2 import PdfWriter, PdfReader
+import io
+
+
 class PdfBuilder:
-    pass
+    def __init__(self):
+        self._pages = {}
+
+    def add_page(self, index, pdf_bytes):
+        self._pages[index] = pdf_bytes
+
+    def clear(self):
+        self._pages.clear()
+
+    def build(self, output_path):
+        writer = PdfWriter()
+        for i in sorted(self._pages.keys()):
+            reader = PdfReader(io.BytesIO(self._pages[i]))
+            for page in reader.pages:
+                writer.add_page(page)
+        with open(output_path, "wb") as f:
+            writer.write(f)
+        self._pages.clear()
+        return output_path
+
+    @property
+    def page_count(self):
+        return len(self._pages)
